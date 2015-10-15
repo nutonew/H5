d25233722c10ba56b632513b870372cc9bdd3039
nutonew <nutonew@hotmail.com>
2015-10-15 19:02:50 +0800

    index

---
+++ h5_web/src/main/java/com/nut/one/IndexController.java
@@ -0,0 +1,15 @@
+package com.nut.one;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by dolphin on 2015/10/15.
+ */
+@Controller
+public class IndexController {
+
+
+    public void index(){
+
+    }
+}
